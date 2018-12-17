7112706b1b8691f8d953028886ce05c16453e96e
scullionw <scuw1801@usherbrooke.ca>
2018-12-17 17:36:04 -0500

    day1 complete

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,29 @@
+use std::collections::HashSet;
+
+const INPUT: &'static str = include_str!("data/input_day1.txt");
+
+fn solve_a(seq: &str) -> i64 {
+    seq.trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .sum()
+}
+
+fn solve_b(seq: &str) -> i64 {
+    let mut set = HashSet::new();
+    seq.trim()
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .cycle()
+        .scan(0, |state, x| {
+            *state += x;
+            Some(*state)
+        })
+        .find(|x| !set.insert(*x))
+        .unwrap()
+}
+
 fn main() {
-    println!("Hello world!");
+    println!("{}", solve_a(INPUT));
+    println!("{}", solve_b(INPUT));
 }
